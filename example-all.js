22cf52794ec4aecc37099d8ef580086483a81f5e
Jannis R <mail@jannisr.de>
2016-03-07 18:11:26 +0100

    show all frames properly

--- example-all.js
+++ example-all.js
@@ -2,20 +2,30 @@
 var logUpdate = require('log-update');
 var cliSpinners = require('./');
 var spinners = Object.keys(cliSpinners);
-var i = 0;
-var j = 0;
-var frames = [];
+var frame = 0;
+var spinner = 0;
+var next;
+
+
+
+var showNextFrame = function () {
+	var frames = cliSpinners[spinners[spinner]].frames;
+	logUpdate(frames[frame++ % frames.length] + ' ' + spinners[spinner]);
+};
+
+var showNextSpinner = function () {
+	if (next) {
+		clearInterval(next);
+		spinner++
+	}
+	if (spinner in spinners) {
+		var s = cliSpinners[spinners[spinner]];
+		next = setInterval(showNextFrame, s.interval || 100);
+		setTimeout(showNextSpinner, (s.interval || 100) * s.frames.length);
+	}
+};
+
+
 
 console.log(spinners.length + ' spinners\n');
-
-setInterval(function () {
-	if (++i === frames.length) {
-		j++;
-		i = 0;
-	}
-
-	frames = cliSpinners[spinners[j]].frames;
-	logUpdate(frames[i] + ' ' + spinners[j]);
-}, 100);
-
-// $ node example.js
+showNextSpinner()
