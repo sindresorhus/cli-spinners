07f792ccb86eb1d2bac98a63ea3920bfd3ca80b0
Guillaume Salicis <gsalicis@fastbooking.net>
2017-12-07 11:40:26 +0100

    Allow user to skip spinner (#4)

--- example-all.js
+++ example-all.js
@@ -1,4 +1,5 @@
 'use strict';
+const readline = require('readline');
 const logUpdate = require('log-update');
 const cliSpinners = require('.');
 
@@ -6,6 +7,24 @@ const spinners = Object.keys(cliSpinners);
 let frame = 0;
 let spinner = 0;
 let next;
+let scrutator;
+
+readline.emitKeypressEvents(process.stdin);
+
+process.stdin.setRawMode(true);
+
+process.stdin.on('keypress', (str, key) => {
+	if(key.sequence === '\u0003') {
+		process.exit();
+	}
+
+	if(key.sequence === '\r') {
+		if (scrutator) {
+			clearTimeout(scrutator);
+			showNextSpinner();
+		}
+	}
+});
 
 const showNextFrame = () => {
 	const frames = cliSpinners[spinners[spinner]].frames;
@@ -21,7 +40,9 @@ const showNextSpinner = () => {
 	if (spinner < spinners.length) {
 		const s = cliSpinners[spinners[spinner]];
 		next = setInterval(showNextFrame, s.interval);
-		setTimeout(showNextSpinner, Math.max(s.interval * s.frames.length, 1000));
+		scrutator = setTimeout(showNextSpinner, Math.max(s.interval * s.frames.length, 1000));
+	} else {
+		process.exit();
 	}
 };
 
