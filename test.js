12df50db0de896c3c47fc13487d2f4b4e9c00c30
nnmrts <nanomiratus@gmail.com>
2021-03-13 01:31:54 +0100

    Added a test for constant width

--- test.js
+++ test.js
@@ -1,8 +1,29 @@
 import test from 'ava';
+import stringLength from 'string-length';
+
 import cliSpinners from '.';
 
 test('main', t => {
 	t.is(typeof cliSpinners, 'object');
 	t.is(cliSpinners.dots.interval, 80);
 	t.true(Array.isArray(cliSpinners.dots.frames));
+});
+
+test('constant width', t => {
+	for (const key of Object.keys(cliSpinners)) {
+		if (key !== 'default') {
+			const {
+				[key]: {
+					frames,
+					frames: [
+						firstFrame
+					]
+				}
+			} = cliSpinners;
+
+			const firstFrameLength = stringLength(firstFrame);
+
+			t.true(frames.every(frame => stringLength(frame) === firstFrameLength));
+		}
+	}
 });
