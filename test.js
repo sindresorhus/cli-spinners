1e1d5327bebde225d495ce36185a8dfd8fa71609
Nano Miratus <nanomiratus@gmail.com>
2021-03-18 13:15:22 +0100

    Add some emoji spinners (#46)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 import test from 'ava';
+import stringLength from 'string-length';
+
 import cliSpinners from '.';
 
 function mockMathRandom(fixedResult) {
@@ -20,6 +22,28 @@ test('main', t => {
 	t.is(typeof cliSpinners, 'object');
 	t.is(cliSpinners.dots.interval, 80);
 	t.true(Array.isArray(cliSpinners.dots.frames));
+});
+
+test('constant width', t => {
+	for (const key of Object.keys(cliSpinners)) {
+		// TODO: Remove this if statement when "module.exports.default = spinners" is removed from index.js.
+		if (key === 'default') {
+			continue;
+		}
+
+		const {
+			[key]: {
+				frames,
+				frames: [
+					firstFrame
+				]
+			}
+		} = cliSpinners;
+
+		const firstFrameLength = stringLength(firstFrame);
+
+		t.true(frames.every(frame => stringLength(frame) === firstFrameLength));
+	}
 });
 
 test('random getter', t => {
